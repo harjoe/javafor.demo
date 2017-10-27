d18fa2c814f207fc7e7e249b7eb18caf37e57435
harjoe <harjoe@hotmail.com>
2017-10-27 12:14:56 +0800

    java.lang.exception

---
+++ java.lang.exception/src/com/javafor/java/lang/exception/Student.java
@@ -0,0 +1,25 @@
+package com.javafor.java.lang.exception;
+
+/**
+ * 
+ *
+ * @author Harjoe; if you have any questions, please contact me with my blog or
+ *         email, thanks.<br>
+ *         blog: <a href="http://www.javafor.com">www.javafor.com</a><br>
+ *         github: <a href="https://github.com/harjoe">www.github.com/harjoe</a><br>
+ *         email: <a href="harjoe@hotmail.com">harjoe@hotmail.com</a><br>
+ **/
+public class Student {
+
+	public String getName() throws Exception {
+
+		throw new Exception("aa");
+
+	}
+
+	public String getAge() {
+
+		throw new RuntimeException("r");
+	}
+
+}
---
+++ java.lang.exception/src/com/javafor/java/lang/exception/Test.java
@@ -0,0 +1,17 @@
+package com.javafor.java.lang.exception;
+
+/**
+ * 
+ *
+ * @author Harjoe; if you have any questions, please contact me with my blog or
+ *         email, thanks.<br>
+ *         blog: <a href="http://www.javafor.com">www.javafor.com</a><br>
+ *         github: <a href="https://github.com/harjoe">www.github.com/harjoe</a><br>
+ *         email: <a href="harjoe@hotmail.com">harjoe@hotmail.com</a><br>
+ **/
+public class Test {
+
+	public static void main(String[] args) {
+
+	}
+}
