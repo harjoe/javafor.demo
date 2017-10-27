9d6afb2f569bdfb15703ef61c2391bd35a29df1e
harjoe <harjoe@hotmail.com>
2017-10-27 11:26:32 +0800

    Revert "jms.activeMQ.helloworld"
    This reverts commit 3383d5947873fddee37eae0ad74baea1cdb8c3d6.

--- java.util.BlockingQueue/src/com/javafor/java/util/BlockingQueue/Tester.java
+++ java.util.BlockingQueue/src/com/javafor/java/util/BlockingQueue/Tester.java
@@ -5,12 +5,8 @@ import java.util.concurrent.BlockingQueue;
 
 /**
  * 
- *
- * @author Harjoe; if you have any questions, please contact me with my blog or
- *         email, thanks.<br>
- *         blog: <a href="http://www.javafor.com">www.javafor.com</a><br>
- *         github: <a href="https://github.com/harjoe">www.github.com/harjoe</a><br>
- *         email: <a href="harjoe@hotmail.com">harjoe@hotmail.com</a><br>
+ * 
+ * @author Harjoe; email: <a href="harjoe@hotmail.com">harjoe@hotmail.com</a>
  **/
 public class Tester {
 
@@ -23,7 +19,7 @@ public class Tester {
 		inputThead.start();
 
 		Thread.sleep(100);
-		// 多个消费统线程
+		//多个消费统线程
 		TakeThread takeThead = null;
 		for (int i = 0; i < 10; i++) {
 			takeThead = new TakeThread();
