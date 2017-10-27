3383d5947873fddee37eae0ad74baea1cdb8c3d6
harjoe <harjoe@hotmail.com>
2017-10-27 10:57:51 +0800

    jms.activeMQ.helloworld

--- java.util.BlockingQueue/src/com/javafor/java/util/BlockingQueue/InputThread.java
+++ java.util.BlockingQueue/src/com/javafor/java/util/BlockingQueue/InputThread.java
@@ -5,7 +5,11 @@ import java.util.concurrent.BlockingQueue;
 /**
  * 创建一个生产数据的线程
  * 
- * @author Harjoe; email: <a href="harjoe@hotmail.com">harjoe@hotmail.com</a>
+ * @author Harjoe; if you have any questions, please contact me with my blog or
+ *         email, thanks.<br>
+ *         blog: <a href="http://www.javafor.com">www.javafor.com</a><br>
+ *         github: <a href="https://github.com/harjoe">www.github.com/harjoe</a><br>
+ *         email: <a href="harjoe@hotmail.com">harjoe@hotmail.com</a><br>
  **/
 public class InputThread extends Thread {
 
--- java.util.BlockingQueue/src/com/javafor/java/util/BlockingQueue/Tester.java
+++ java.util.BlockingQueue/src/com/javafor/java/util/BlockingQueue/Tester.java
@@ -5,8 +5,12 @@ import java.util.concurrent.BlockingQueue;
 
 /**
  * 
- * 
- * @author Harjoe; email: <a href="harjoe@hotmail.com">harjoe@hotmail.com</a>
+ *
+ * @author Harjoe; if you have any questions, please contact me with my blog or
+ *         email, thanks.<br>
+ *         blog: <a href="http://www.javafor.com">www.javafor.com</a><br>
+ *         github: <a href="https://github.com/harjoe">www.github.com/harjoe</a><br>
+ *         email: <a href="harjoe@hotmail.com">harjoe@hotmail.com</a><br>
  **/
 public class Tester {
 
@@ -19,7 +23,7 @@ public class Tester {
 		inputThead.start();
 
 		Thread.sleep(100);
-		//多个消费统线程
+		// 多个消费统线程
 		TakeThread takeThead = null;
 		for (int i = 0; i < 10; i++) {
 			takeThead = new TakeThread();
