117f162bea12633ff56e5459e45e5d63aabda226
harjoe <harjoe@hotmail.com>
2017-10-27 12:08:29 +0800

    java.util.ArrayList

---
+++ java.util.ArrayList/src/com/javafor/java/util/ArrayList/Tester.java
@@ -0,0 +1,34 @@
+package com.javafor.java.util.ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ *
+ * @author Harjoe; if you have any questions, please contact me with my blog or
+ *         email, thanks.<br>
+ *         blog: <a href="http://www.javafor.com">www.javafor.com</a><br>
+ *         github: <a href="https://github.com/harjoe">www.github.com/harjoe</a><br>
+ *         email: <a href="harjoe@hotmail.com">harjoe@hotmail.com</a><br>
+ **/
+public class Tester {
+
+	public static void main(String[] args) {
+		
+		System.out.println("www.itwhile.com start.....");
+		
+		//TODO
+		
+		List<String> names = new ArrayList<String>();
+		
+		names.add("bridge");
+		names.add("harjoe");
+		String[] ss = names.toArray(new String[0]);
+		
+		for(String name : ss)
+			System.out.println(name);
+		
+		System.out.println("www.itwhile.com end....");
+	}
+}
--- jms.activeMQ.helloworld/src/com/javafor/jms/activeMQ/helloworld/Producer.java
+++ jms.activeMQ.helloworld/src/com/javafor/jms/activeMQ/helloworld/Producer.java
@@ -1,10 +1,10 @@
-package jms.activeMQ.helloworld;
+package com.javafor.jms.activeMQ.helloworld;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.JMSException;
-import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
@@ -20,36 +20,37 @@ import org.apache.activemq.ActiveMQConnectionFactory;
  *         github: <a href="https://github.com/harjoe">www.github.com/harjoe</a><br>
  *         email: <a href="harjoe@hotmail.com">harjoe@hotmail.com</a><br>
  **/
-public class Consumer {
+public class Producer {
 
 	public static void main(String[] args) throws JMSException {
-		// 前提,你得装了ActiveMQ, 用得默认配置
 		String username = ActiveMQConnection.DEFAULT_USER;
 		String password = ActiveMQConnection.DEFAULT_PASSWORD;
 		String url = ActiveMQConnection.DEFAULT_BROKER_URL;
+		System.out.println(username);
+		System.out.println(password);
+		System.out.println(url);
 
 		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(username, password, url);
+
 		Connection connection = connectionFactory.createConnection();
 		connection.start();
-		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
 		Destination destination = session.createQueue("hello");
-		MessageConsumer consumer = session.createConsumer(destination);
 
-		System.out.println("receive...");
+		MessageProducer messageProducer = session.createProducer(destination);
 
-		while (true) {
-			TextMessage message = (TextMessage) consumer.receive(100000);
-			if (message != null)
-				System.out.println("receive: " + message.getText());
-			else {
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < 10000; i++) {
+			// ObjectMessage objectMessage = session.createObjectMessage();
+			TextMessage message = session.createTextMessage("message " + i);
 
-				System.out.println("blank");
-				break;
-			}
-
+			// System.out.println("send: " + message.getText());
+			messageProducer.send(message);
+			// objectMessage.setObject(arg0);
 		}
+		session.commit();
 
-		System.out.println("receive finished.");
+		long end = System.currentTimeMillis();
+		System.out.println(end - start);
 	}
-
 }
